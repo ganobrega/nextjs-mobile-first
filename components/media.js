763fd7024c72ceaa83e59fa0eb633c004640040d
Gabriel Nobrega <gnobrega.contato@gmail.com>
2020-07-28 02:09:23 -0300

    fix: removing unused useEffect

--- components/media.js
+++ components/media.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { createMedia } from '@artsy/fresnel';
 
 export const Context = React.createContext();
