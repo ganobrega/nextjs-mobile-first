ccdf2d4919a798e695830cd3a058f7c99a626949
Gabriel Nobrega <gnobrega.contato@gmail.com>
2020-07-28 02:23:50 -0300

    fix: heroku prepare

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
 const device = require('device');
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-        const parsedUrl = parse(req.url, true)
+    const parsedUrl = parse(req.url, true);
 
-        const _device = device(req.headers['user-agent']);
+    const _device = device(req.headers['user-agent']);
 
-        req.device = _device.type;
+    req.device = _device.type;
 
-        handle(req, res, parsedUrl)
-    }).listen(3000, (err) => {
-        if (err) throw err
-        console.log('> Ready on http://localhost:3000')
-    })
-})
+    handle(req, res, parsedUrl);
+  }).listen(process.env.PORT, (err) => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3000');
+  });
+});
